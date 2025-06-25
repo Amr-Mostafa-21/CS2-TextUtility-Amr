ae32e18e5a15cb7b415a11d2389b15c4da0faaa7
Amr_Mostafa <amr_21@aucegypt.edu>
2025-06-25 14:27:59 +0300

    the headers file is created

---
+++ text_utility.h
@@ -0,0 +1,20 @@
+#ifndef TEXT_UTILITY_H
+#define TEXT_UTILITY_H
+
+int word_count(char arr[]);
+
+int Character_Count(char arr[]);
+
+void Vowel_Consonant_Count(char arr[], int& v, int& co);
+
+void String_Reversal(char arr[]);
+
+bool isPalindrome(char arr[]);
+
+void to_upper_case(char arr[]);
+
+void to_lower_case(char arr[]);
+
+int substringCount(char str[], char sub[]);
+
+#endif
